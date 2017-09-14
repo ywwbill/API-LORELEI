b1bbef08a29b82a7955b102b4e9c3ed5af11f16c
Weiwei Yang <weiwei.yang.1990@gmail.com>
2017-09-14 11:25:13 -0400

    ID field name alternative

--- src/main/java/edu/umd/lorelei/utils/InputDoc.java
+++ src/main/java/edu/umd/lorelei/utils/InputDoc.java
@@ -1,7 +1,10 @@
 package edu.umd.lorelei.utils;
 
+import com.google.gson.annotations.SerializedName;
+
 public class InputDoc
 {
+	@SerializedName(value="ID", alternate= {"id", "Id", "iD"})
 	public String ID;
 	public String content;
 	public int label;
